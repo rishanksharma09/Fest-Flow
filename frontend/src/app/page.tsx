8c46be85cbc1a8d2e22b5ce6714066a618330b64
Rishank Sharma <rishanksharma04524@gmail.com>
2026-01-21 23:29:26 +0530

    mode toggle added

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,11 @@
-
+import ModeToggle from "./components/ModeToggle";
 export default function Home() {
   return (
     <>
     <div className="font-heading font-bold text-7xl">hi hello how are you</div>
     <div className="font-body  text-2xl">hi hello how are you</div>
+    <ModeToggle/>
+    
     </>
   );
 }
